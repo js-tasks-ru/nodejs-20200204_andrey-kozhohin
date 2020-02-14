61a11602407abab51004056f253c87b58a5cc650
User <akpost@gmail.com>
2020-02-14 15:18:11 +0300

    module 1 and 2

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._nextStr = '';
   }
 
   _transform(chunk, encoding, callback) {
-  }
+    const chunkArr = chunk.toString().split(`${os.EOL}`);
+
+    chunkArr.forEach((string, i) => {
+
+      if (chunkArr.length === i + 1) { // запоминаем последний или единственный эл массива
+        this._nextStr += string;
+      } else {
+        this.push(this._nextStr += string); // прибавляем к нему следующий эл и пушим
+        this._nextStr = '';
+      }
+    });
+
+    callback();
+  };
+
 
   _flush(callback) {
+    // вызывается один раз в конце обработки чанка, пушит последний сложенный эл
+    callback(null, this._nextStr);
   }
 }
 
