4a0a5d0ea4b8a33125f5a8e80be1d3267621a470
User <akpost@gmail.com>
2020-02-23 17:31:26 +0300

    module 5-1

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,11 +8,39 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+let clients = [];
+
+router.get('/subscribe', async (ctx, next) => { // получение сообщений. запрос подвешивается
+
+    ctx.set('Content-Type', 'text/plain;charset=utf-8');
+    ctx.set("Cache-Control", "no-cache, must-revalidate");
+
+    // await останавливает выполнение кода пока не будет получен ответ от промиса
+    await new Promise((resolve, reject) => {
+        // const id = ctx.request.query.r;
+        clients.push(resolve); // функцию ответа этого промиса сохраняем в массив
+
+    }).then(message => {
+        //ctx.status = 200;
+        ctx.body = message;
+    });
 
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx, next) => { // отправка сообщений
+    const message = ctx.request.body.message;
+
+    if (!message) {
+        ctx.throw(400);
+    }
+
+    clients.forEach((resolve) => {
+        resolve(String(message)); // вызываем функцию ответа промиса и передаем в него сообщение
+    });
+
+    clients = [];
+    //ctx.status = 200;
+    ctx.body = 'done';
 
 });
 
