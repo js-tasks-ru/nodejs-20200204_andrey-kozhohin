a1345d61b0195295158f5e8d0810eb64ebe5f0b4
User <akpost@gmail.com>
2020-02-26 17:01:28 +0300

    module 6-2

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,43 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+
+    const subCategory = ctx.request.query.subcategory;
+    if (subCategory) {
+        let productList = await Product.find({subcategory: subCategory});
+
+        productList = productList.map((doc) => {
+            return doc.toObject();
+        });
+
+        ctx.body = {products: productList};
+    } else {
+        await next();
+    }
 };
+
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+
+    let productListArr = await Product.find({});
+
+    productListArr = productListArr.map((doc) => {
+        return doc.toObject();
+    });
+
+    ctx.body = {products: productListArr};
 };
 
+
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+
+  if (mongoose.Types.ObjectId.isValid(ctx.params.id)) { // валидация ObjectId
+        const prod = await Product.find({_id: ctx.params.id});
+        if (prod.length === 0) return ctx.status = 404;
+        return ctx.body = {product: prod[0].toObject()};
+    }
+    ctx.status = 400;
 };
 
--- 6-module/2-task/models/Category.js
+++ 6-module/2-task/models/Category.js
@@ -6,7 +6,7 @@ const subCategorySchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-});
+}, {versionKey: false, toObject: {virtuals: true}});
 
 const categorySchema = new mongoose.Schema({
   title: {
@@ -15,6 +15,32 @@ const categorySchema = new mongoose.Schema({
   },
 
   subcategories: [subCategorySchema],
-});
+}, {versionKey: false, toObject: {virtuals: true}});
+
+/*
+categorySchema.virtual('id')
+    .get(function() {
+      return this._id;
+    });
+
+subCategorySchema.virtual('id')
+    .get(function() {
+      return this._id;
+    });
+*/
+
+if (!categorySchema.options.toObject) categorySchema.options.toObject = {};
+categorySchema.options.toObject.transform = function(doc, ret, options) {
+  ret.id = ret._id;
+  delete ret._id;
+  return ret;
+};
+
+if (!subCategorySchema.options.toObject) subCategorySchema.options.toObject = {};
+subCategorySchema.options.toObject.transform = function(doc, ret, options) {
+  ret.id = ret._id;
+  delete ret._id;
+  return ret;
+};
 
 module.exports = connection.model('Category', categorySchema);
