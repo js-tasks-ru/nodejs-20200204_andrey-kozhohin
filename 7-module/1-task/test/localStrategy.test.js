97a5ce9f3e5a26aa0a9d6ff3f6da555db2482f0f
Anton Griadchenko <anton_griadchenko@epam.com>
2020-02-28 15:55:37 +0200

    Add fixtures

--- 7-module/1-task/test/localStrategy.test.js
+++ 7-module/1-task/test/localStrategy.test.js
@@ -2,7 +2,7 @@ const connection = require('../libs/connection');
 const localStrategy = require('../libs/strategies/local');
 const expect = require('chai').expect;
 const User = require('../models/User');
-const users = require('../fixtures/users');
+const users = require('../../../__data/users');
 
 describe('7-module-1-task', () => {
   describe('passport local strategy', function() {
