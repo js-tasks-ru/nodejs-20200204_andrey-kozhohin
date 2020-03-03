c7f5a6c49189f27a2118ffa90be8ad3812d61e36
User <akpost@gmail.com>
2020-03-03 18:40:29 +0300

    module 7-1

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,33 @@
+const User = require('../../models/User');
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
-};
+  if (email) {
+    async function getUser() {
+      return await User.findOne({email: email});
+    }
+
+    async function saveUser(user) {
+      try {
+        return await user.save({validateBeforeSave: true});
+      } catch (error) {
+        done(error, false);
+      }
+    }
+
+    getUser().then((result) => {
+      if (result) {
+        done(null, result);
+      } else {
+        const user = new User({
+          email: email,
+          displayName: displayName,
+        });
+        saveUser(user).then((result) => {
+          if (result) done(null, result);
+        });
+      }
+    });
+  } else {
+    done(null, false, `Не указан email`);
+  }
+}
+;
